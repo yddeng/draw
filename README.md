6f33c1ba2f7520c87b79b84c4b8a21df59b84dea
yidongdeng <248244142@qq.com>
2021-04-23 18:25:40 +0800

    info

--- README.md
+++ README.md
@@ -1,12 +1,61 @@
-# DrawCard
+# Draw
 
-抽卡、抽奖、掉落库
+抽卡、抽奖、掉落库 drawcard 、lottery
 
-drawcard 、lottery
+## Interface
 
-## 支持
+```
+type Interface interface {
+	// Len is the number of elements in the collection.
+	Len() int
 
-1. 物品全掉落。
-2. 物品随机产出。
+	// Weight returns weight of the elements at index i.
+	Weight(i int) int
+}
+```
 
-池中池嵌套组合。
+## Usage
+
+Rand returns the index of elements
+
+`func Rand(data Interface, count int, repeated bool) []int `
+
+RandRangeInt returns as an int, a non-negative pseudo-random number in [min,max]
+
+`func RandRangeInt(min, max int) int`
+
+## Example
+
+```
+type Slice struct {
+	items []*Item
+}
+
+type Item struct {
+	ID     string
+	Weight int
+}
+
+func (s *Slice) Len() int {
+	return len(s.items)
+}
+
+func (s *Slice) Weight(i int) int {
+	return s.items[i].Weight
+}
+
+func TestRand(t *testing.T) {
+	s := &Slice{items: []*Item{
+		{ID: "11", Weight: 10},
+		{ID: "22", Weight: 15},
+		{ID: "33", Weight: 20},
+		{ID: "44", Weight: 25},
+		{ID: "55", Weight: 30},
+	}}
+
+	// repeated
+	result := Rand(s, 3, true)
+	fmt.Println(result)
+}
+
+```
--- draw.go
+++ draw.go
@@ -2,7 +2,7 @@ package draw
 
 import "math/rand"
 
-// Pool for drawcard
+// Interface
 type Interface interface {
 	// Len is the number of elements in the collection.
 	Len() int
@@ -17,7 +17,7 @@ func totalWeight(data Interface) int {
 	for i := 0; i < data.Len(); i++ {
 		weight := data.Weight(i)
 		if weight <= 0 {
-			panic("draw:totalWeight weight value failed, it's must 0")
+			panic("draw:totalWeight weight value failed, it's must be greater than 0")
 		}
 		w += data.Weight(i)
 	}
@@ -32,18 +32,18 @@ func slice(length int) []int {
 	return slice
 }
 
-// randRange returns as an int, a non-negative pseudo-random number in [min,max]
-func randRange(min, max int) int {
+// RandRangeInt returns as an int, a non-negative pseudo-random number in [min,max]
+func RandRangeInt(min, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
 // Rand returns the index of elements
-// it return all if count >= data.length and !repeated
 func Rand(data Interface, count int, repeated bool) []int {
 	if count == 0 || data.Len() == 0 {
 		return nil
 	}
 
+	// it return all if count >= data.length and !repeated
 	result := make([]int, 0, count)
 	if !repeated && count >= data.Len() {
 		return slice(data.Len())
@@ -56,7 +56,7 @@ func Rand(data Interface, count int, repeated bool) []int {
 	)
 
 	for curCount < count {
-		weight := randRange(1, totalWeight)
+		weight := RandRangeInt(1, totalWeight)
 		for i, idx := range index {
 			weight -= data.Weight(idx)
 			if weight <= 0 {
@@ -73,3 +73,5 @@ func Rand(data Interface, count int, repeated bool) []int {
 
 	return result
 }
+
+func PoolRand(data interface{}) {}
